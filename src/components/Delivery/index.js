d87223cfd23b76abe268fafbb38e106bb5ee23b9
prashanth <prashanthkore234@gmail.com>
2022-03-09 05:36:05 +0000

    nxtTrendsApp

---
+++ src/components/Delivery/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const Delivery = () => (
+  <div className="not-found-container">
+    <img
+      src="https://cdn.pixabay.com/photo/2020/09/14/20/39/vans-5572117_960_720.png"
+      alt="not found"
+      className="not-found-img"
+    />
+    <h1>Your Product is Delivered Soon...</h1>
+  </div>
+)
+
+export default Delivery
